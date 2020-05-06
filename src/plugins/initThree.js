5da9f35563acb63b875823bc77027d3df50ae916
daiya <phaosdove@gmail.com>
2020-05-06 14:31:38 +0900

    関数を切り出せていないが動く

--- src/plugins/initThree.js
+++ src/plugins/initThree.js
@@ -1,50 +1,48 @@
 import initARJS from './initAR'
 
-const clock = new THREE.Clock()
 
-const addFlamingoObject = (scene, onRenderFcts) => {
-  const root = new THREE.Object3D()
-  scene.add(root)
-  /**
-   * add an object in the scene
-   */
-  const threeGLTFLoader = new THREE.GLTFLoader()
-  const mixers = []
-  threeGLTFLoader.load('resources/Flamingo.glb', (gltf) => {
-    const model = gltf.scene.children[0]
-    model.name = 'Flamingo'
+// const addFlamingoObject = async (scene, onRenderFcts) => {
+//   const root = new THREE.Object3D()
+//   scene.add(root)
+//   /**
+//    * add an object in the scene
+//    */
+//   const threeGLTFLoader = new THREE.GLTFLoader()
+//   const mixers = []
+//   await threeGLTFLoader.load('resources/Flamingo.glb', (gltf) => {
+//     const model = gltf.scene.children[0]
+//     model.name = 'Flamingo'
 
-    const animation = gltf.animations[0]
-    const mixer = new THREE.AnimationMixer(model)
-    mixers.push(mixer)
-    onRenderFcts.push(() => {
-      if (mixers.length > 0) {
-        // eslint-disable-next-line no-plusplus
-        for (let i = 0; i < mixers.length; i++) {
-          mixers[i].update(clock.getDelta())
-        }
-      }
-    })
-    const action = mixer.clipAction(animation)
-    action.play()
+//     const animation = gltf.animations[0]
+//     const mixer = new THREE.AnimationMixer(model)
+//     mixers.push(mixer)
+//     onRenderFcts.push((delta) => {
+//       if (mixers.length > 0) {
+//         // eslint-disable-next-line no-plusplus
+//         for (let i = 0; i < mixers.length; i++) {
+//           mixers[i].update(delta)
+//         }
+//       }
+//     })
+//     const action = mixer.clipAction(animation)
+//     action.play()
 
-    root.matrixAutoUpdate = false
-    root.add(model)
+//     root.matrixAutoUpdate = false
+//     root.add(model)
 
-    model.position.z = -200
-    model.position.x = 100
-    model.position.y = 100
-  })
-}
+//     model.position.z = -200
+//     model.position.x = 100
+//     model.position.y = 100
+//   })
+// }
 
-const init = () => {
+const init = async () => {
   const renderer = new THREE.WebGLRenderer({
     antialias: true,
     alpha: true,
     precision: 'mediump',
   })
-
-
+  const clock = new THREE.Clock()
   const onRenderFcts = []
 
   renderer.setPixelRatio(window.devicePixelRatio)
@@ -68,15 +66,43 @@ const init = () => {
   scene.add(light)
 
   const smoothedRoot = initARJS(scene, camera, onRenderFcts, renderer)
-  addFlamingoObject(smoothedRoot, onRenderFcts)
-  onRenderFcts.push(() => renderer.render(scene, camera))
 
-  const animate = () => {
-    // keep looping
+  const root = new THREE.Object3D()
+  scene.add(root)
+  /**
+   * add an object in the scene
+   */
+  const threeGLTFLoader = new THREE.GLTFLoader()
+  const mixers = []
+  threeGLTFLoader.load('resources/Flamingo.glb', (gltf) => {
+    const model = gltf.scene.children[0]
+    model.name = 'Flamingo'
+
+    const animation = gltf.animations[0]
+    const mixer = new THREE.AnimationMixer(model)
+    mixers.push(mixer)
+    const action = mixer.clipAction(animation)
+    action.play()
+
+    root.matrixAutoUpdate = false
+    root.add(model)
+
+    model.position.z = -200
+    model.position.x = 100
+    model.position.y = 100
+    const animate = () => {
+      requestAnimationFrame(animate)
+      onRenderFcts.forEach(onRenderFct => onRenderFct(clock.getDelta()))
+      if (mixers.length > 0) {
+        // eslint-disable-next-line no-plusplus
+        for (let i = 0; i < mixers.length; i++) {
+          mixers[i].update(clock.getDelta())
+        }
+      }
+      renderer.render(scene, camera)
+    }
     requestAnimationFrame(animate)
-    onRenderFcts.forEach(onRenderFct => onRenderFct(clock.getDelta()))
-  }
-  requestAnimationFrame(animate)
+  })
 }
 
 export default init
